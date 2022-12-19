0ca85723bc9d5c6f6f0058677e6abadb5eb29679
Artormvladis <112126702+Artormvladis@users.noreply.github.com>
2022-12-19 12:54:45 +0300

    Create middle_func2.cpp

---
+++ middle_func2.cpp
@@ -0,0 +1,80 @@
+#include "easy_func.h"
+#include "middle_func.h"
+string itc_maxCharWord(string str)
+{
+    long long max=0, len=itc_len(str);
+    string a1,a2;
+    if (itc_countWords(str)==1) 
+      return "error";
+    for (int i=0; i<len; ++i)
+    {
+        if (((str[i]>='A') && (str[i]<='Z')) || ((str[i]>='a') && (str[i]<='z')))
+            a1+=str[i];
+        else if (itc_len(a1)>max)
+        {
+            max=itc_len(a);
+            a2=a1;
+            a1="";
+        }
+        else
+            a1="";
+    }
+    if (itc_len(a1)>max)
+        a2=a1;
+    return a2;
+}
+char itc_sameChar(string str)
+{
+    for (long long i=0; str[i]!='\0'; ++i)
+    {
+        if ((str[i]>='A' && str[i]<='Z') || (str[i]>='a' && str[i]<='z'))
+        {
+            for (long long a1=0; str[a1]!='\0'; a1++)
+                if (str[a1]==str[i])
+                    return str[a1];
+        }
+    }
+    return '0';
+}
+
+bool itc_isFirstInSecond(string a1, string a2){
+    return itc_find_str(a1, a2)==-1;
+}
+string itc_slice_str(string str, int a1, int a2){
+    string res="";
+    if (a2>=itc_len(str))
+        a2=itc_len(str)-1;
+    if(a1>a2){
+        return str;
+    }else if(a1==(itc_len(str)-1)){
+        res+=str[itc_len(str)-1];
+        return res;
+    }else{
+        for (long long i=a1; i<=a2; i++){
+            res+=str[i];
+        }
+        return res;
+    }
+    return "-1";
+}
+string itc_Cezar(string str, int a1)
+{
+    if (a1 == 0) return str;
+    int i = 0;
+    string b1="";
+    while (str[i] != '\0') {
+        if (str[i]>='a' && str[i]<='z') {
+            if (str[i]+a1>'z') b1+=str[i]+a1-26;
+            else if (str[i]+a1<'a') b1+=str[i]+a1+26;
+            else b1+=str[i]+a1;
+        }
+        else if (str[i]>='A' && str[i]<='Z') {
+            if (str[i]+a1>'Z') b1+=str[i]+a1-26;
+            if (str[i]+a1<'A') b1+=str[i]+a1+26;
+            else b1+=str[i]+a1;
+        }
+        else b1+=str[i];
+        i+=1;
+    }
+    return b1;
+}
