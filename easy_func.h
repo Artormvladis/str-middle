062516b709bef1788c7f38fe9fa6967e9cb51f39
Artormvladis <112126702+Artormvladis@users.noreply.github.com>
2022-12-19 12:33:48 +0300

    Create easy_func.h

---
+++ easy_func.h
@@ -0,0 +1,9 @@
+#ifndef MIDDLE_STR_H_INCLUDED
+#define MIDDLE_STR_H_INCLUDED
+#include <iostream>
+#include <string.h>
+using namespace std;
+long long itc_len(string str);
+int itc_find_str(string str1, string str2);
+string itc_slice_str(string str, int start, int endd);
+#endif // MIDDLE_STR_H_INCLUDED
