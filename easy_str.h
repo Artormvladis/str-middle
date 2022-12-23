69ea806f30e00363ce0c46132de392ed4d5c9fb6
Artormvladis <112126702+Artormvladis@users.noreply.github.com>
2022-12-23 14:46:10 +0300

    Update easy_str.h

--- easy_str.h
+++ easy_str.h
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <string.h>
 using namespace std;
-long long itc_len(string str);
+long long itc_len(string str1);
 int itc_find_str(string str1, string str2);
 string itc_slice_str(string str, int a1, int a2);
 #endif // MIDDLE_STR_H_INCLUDED
