a0f2c60794b03db170ddad60003618bae69f2c97
Artormvladis <112126702+Artormvladis@users.noreply.github.com>
2022-12-23 14:43:17 +0300

    Update middle_str.h

--- middle_str.h
+++ middle_str.h
@@ -10,7 +10,7 @@ bool itc_compare(string s1, string s2);
 int itc_countWords(string str);
 string itc_maxCharWord(string str);
 char itc_sameChar(string str);
-bool itc_isFirstInSecond(string s1, string s2);
+bool itc_isFirstInSecond(string a1, string a2);
 string itc_Cezar(string str, int k);
 string itc_rmFreeSpace(string str);
 bool itc_isIp(string str);
