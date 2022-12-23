aa99b5fed4c7d06243c1afed7cc472951205278a
Artormvladis <112126702+Artormvladis@users.noreply.github.com>
2022-12-23 12:03:55 +0300

    Update easy_func.cpp

--- easy_func.cpp
+++ easy_func.cpp
@@ -13,9 +13,9 @@ int itc_find_str(string str1, string str2) {
     if (str1[i]==str2[0]) {
       long long a1=i;
       long long a2=0;
-      for (a2<len2 && a1<len1; a2++, a1++) {
-        if (str1[a1]!=str2[a2])
-          break;
+      while (a2<len2 && a1<len1) {
+        a1++;
+        a2++;
       }
       if (a2==len2)
         return i;
