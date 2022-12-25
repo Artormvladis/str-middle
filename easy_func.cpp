f9e456e51e5683a1e494dd69d149c1d1907a3955
Artormvladis <112126702+Artormvladis@users.noreply.github.com>
2022-12-25 21:36:20 +0300

    Update easy_func.cpp

--- easy_func.cpp
+++ easy_func.cpp
@@ -40,3 +40,39 @@ string itc_slice_str(string str, int a1, int a2){
     }
     return "-1";
 }
+string itc_covert_num(int a1, int a2)
+{
+	string b1 = "", b2 = "";
+	if ((a2 > 16) || (a2 < 2) || (a1 < 0))
+		return "-1";
+	
+	else if (a2 < 10) {
+		while (a1> 0) {
+			b1 += ('0' + (a1 % a2));
+			a1 /= a2;
+		}
+	}
+	else {
+		while (a1 > 0) {
+			if (a1 % a2 >= 10) b1 += ('A' + (a1 % a2) - 10);
+			else b1 += ('0' + (a1 % a2));
+			a1 /= a2;
+		}
+	}
+  int i = itc_len(otv1);
+	while (i > 0) {
+		i--;
+		b2 += b1[i];
+	}
+	return b2;
+}
+string itc_num_to_str(long long a1){
+    char b = ' ';
+    string c = "";
+    while(a1 > 0){
+        b = (a1 % 10) + 48;
+        c =  b + c;
+        a1 /= 10;
+    }
+    return c;
+}
