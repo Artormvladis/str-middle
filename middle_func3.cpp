b7b9087683e41b37c8fc7bfa3039b05d154ba314
Artormvladis <112126702+Artormvladis@users.noreply.github.com>
2022-12-26 14:54:27 +0300

    Update middle_func3.cpp

--- middle_func3.cpp
+++ middle_func3.cpp
@@ -19,22 +19,6 @@ bool itc_isIp(string str)
         return 0;
 	return 1;
 }
-
-string itc_DecToBin(string str){
-    string a1="", a2="";
-    long long a1_num=0;
-    for(long long i=0; str[i]!='\0'; ++i){
-        if (str[i]<'0' || str[i]>'9'){
-            a2+=itc_decToBase(str_to_num(a1), 2)+str[i];
-            a1="";
-        }
-        if (itc_isDigit(str[i]))
-            a1+=str[i];  
-    }
-
-    a2+=itc_decToBase(str_to_num(a1), 2);
-    return a2;
-}
 string itc_decToBase(int a1, int a2){
     string b1="0123456789ABCDEF";
     string res="";
@@ -49,4 +33,19 @@ string itc_decToBase(int a1, int a2){
         a1/=a2;
     }
     return res;
+}
+string itc_DecToBin(string str){
+    string a1="", a2="";
+    long long a1_num=0;
+    for(long long i=0; str[i]!='\0'; ++i){
+        if (str[i]<'0' || str[i]>'9'){
+            a2+=itc_decToBase(str_to_num(a1), 2)+str[i];
+            a1="";
+        }
+        if (itc_isDigit(str[i]))
+            a1+=str[i];  
+    }
+
+    a2+=itc_decToBase(str_to_num(a1), 2);
+    return a2;
 }
