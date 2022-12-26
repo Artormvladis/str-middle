1e34e7181f9bd9032856e1da85146d72bae91b37
Artormvladis <112126702+Artormvladis@users.noreply.github.com>
2022-12-26 15:10:30 +0300

    Update middle_func1.cpp

--- middle_func1.cpp
+++ middle_func1.cpp
@@ -1,13 +1,5 @@
 #include "easy_str.h"
 #include "middle_str.h"
-bool itc_isDigit(unsigned char a1)
-{
-	return((a1>=48) && (a1<=57));
-}
-unsigned char itc_toUpper(unsigned char a1){
-    if(a1>=97 && a1<=122) return a1-32;
-    return a1;
-}
 unsigned char itc_changeCase(unsigned char a1){
     if (a1<='z' && a1>='a')
         return a1-' ';
