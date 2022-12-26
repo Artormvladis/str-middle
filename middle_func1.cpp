4a464483798e953e34a50adc7880f8d62f957a12
Artormvladis <112126702+Artormvladis@users.noreply.github.com>
2022-12-26 23:40:09 +0300

    Update middle_func1.cpp

--- middle_func1.cpp
+++ middle_func1.cpp
@@ -1,44 +1,41 @@
 #include "easy_str.h"
 #include "middle_str.h"
-unsigned char itc_changeCase(unsigned char a1){
-    if (a1<='z' && a1>='a')
-        return a1-' ';
-    else if(a1<='Z' && a1>='A')
-        return a1+' ';
-    else
-        return a1;
+long long itc_len(string str) {
+  long long kol = 0;
+  for (int i = 0; str[i] != '\0'; i++)
+    kol++;
+  
+  return kol;
 }
-bool itc_compare(string a1, string a2){
-    for (long long i=0; i<=itc_len(a1); i++) {
-        if (a1[i]!=a2[i])
+
+
+bool itc_isDigit(unsigned char c)
+{
+	return((c >= 48) && (c <= 57));
+}
+
+unsigned char itc_toUpper(unsigned char c){
+    if(c >= 97 && c <= 122) return c - 32;
+    return c;
+}
+
+
+unsigned char itc_changeCase(unsigned char c){
+    if ( c <= 'z' && c >= 'a')
+        return c - ' ';
+    else if(c <= 'Z' && c >= 'A')
+        return c + ' ';
+    else
+        return c;
+}
+
+bool itc_compare(string s1, string s2){
+    for (long long i = 0; i <= itc_len(s1); i++) {
+        if (s1[i] != s2[i]) {
             return false;
+            break;
+        }
+
     }
     return true;
-}
-char itc_sameChar(string str)
-{
-    for (long long i=0; str[i]!='\0'; ++i)
-    {
-        if ((str[i]>='A' && str[i]<='Z') || (str[i]>='a' && str[i]<='z'))
-        {
-            for (long long a1=0; str[a1]!='\0'; a1++)
-                if (str[a1]==str[i])
-                    return str[a1];
-        }
-    }
-    return '0';
-}
-string itc_rmFreeSpace(string str)
-{
-    long long len=itc_len(str);
-	string a1="", res="";
-	for (int i=0; i<len; i++)
-        if (!((str[i]==' ') && (str[i+1]==' ')))
-            a1+=str[i];
-	len=itc_len(a1);
-	for (int a2=0; a2<len; a2++)
-        if (!((a2==len-1) && (a1[a2]==' ')))
-            res+=a1[a2];
-	return res;
-
 }
