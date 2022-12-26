7fa514d32cd553f59b66f8274d78ffe3d5eee658
Artormvladis <112126702+Artormvladis@users.noreply.github.com>
2022-12-26 13:55:49 +0300

    Update easy_func2.cpp

--- easy_func2.cpp
+++ easy_func2.cpp
@@ -4,7 +4,7 @@ long long str_to_num(string a1){
     int b = 0;
     long long a2 = 0;
     for (long long i = 0; a1[i] != '\0'; ++i){
-        ch = a1[i] - 48;
+        b = a1[i] - 48;
         a2 = a2 * 10 + b;
     }
     return a2;
