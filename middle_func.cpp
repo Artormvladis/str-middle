a493e086ce1a892d40472abebda1834c3d0c824e
Artormvladis <112126702+Artormvladis@users.noreply.github.com>
2022-12-26 23:37:50 +0300

    Update middle_func.cpp

--- middle_func.cpp
+++ middle_func.cpp
@@ -1,61 +1,48 @@
-#include "easy_str.h"
 #include "middle_str.h"
-long long itc_len(string str1) {
-  long long kol=0;
-  for (int i=0; str1[i]!='\0'; i++)
-    kol++;
-  return kol;
-}
-string itc_slice_str(string str, int a1, int a2){
-    string res="";
-    if (a2>= itc_len(str))
-        a2=itc_len(str)-1;
-    if(a1>a2){
-        return str;
-    }else if(a1==(itc_len(str) - 1)){
-        res+=str[itc_len(str) - 1];
-        return res;
-    }else{
-        for (long long i=a1; i<=a2; i++){
-            res+=str[i];
-        }
-        return res;
-    }
-    return "-1";
-}
-string itc_covert_num(int a1, int a2)
+string itc_covert_num(int number, int ss)
 {
-	string b1 = "", b2 = "";
-	if ((a2 > 16) || (a2 < 2) || (a1 < 0))
+	string otv1 = "", otv2 = "";
+	if ((ss > 16) || (ss < 2) || (number < 0))
 		return "-1";
 	
-	else if (a2 < 10) {
-		while (a1> 0) {
-			b1 += ('0' + (a1 % a2));
-			a1 /= a2;
+	else if (ss < 10) {
+		while (number > 0) {
+			otv1 += ('0' + (number % ss));
+			number /= ss;
 		}
 	}
 	else {
-		while (a1 > 0) {
-			if (a1 % a2 >= 10) b1 += ('A' + (a1 % a2) - 10);
-			else b1 += ('0' + (a1 % a2));
-			a1 /= a2;
+		while (number > 0) {
+			if (number % ss >= 10) otv1 += ('A' + (number % ss) - 10);
+			else otv1 += ('0' + (number % ss));
+			number /= ss;
 		}
 	}
-  int i = itc_len(b1);
+	int i = itc_len(otv1);
 	while (i > 0) {
 		i--;
-		b2 += b1[i];
+		otv2 += otv1[i];
 	}
-	return b2;
+	return otv2;
 }
-string itc_num_to_str(long long a1){
-    char b = ' ';
-    string c = "";
-    while(a1 > 0){
-        b = (a1 % 10) + 48;
-        c =  b + c;
-        a1 /= 10;
+
+string itc_num_to_str(long long num){
+    char ch = ' ';
+    string str = "";
+    while(num > 0){
+        ch = (num % 10) + 48;
+        str =  ch + str;
+        num /= 10;
     }
-    return c;
+    return str;
+}
+
+long long str_to_num(string temp){
+    int ch = 0;
+    long long temp_num = 0;
+    for (long long i = 0; temp[i] != '\0'; ++i){
+        ch = temp[i] - 48;
+        temp_num = temp_num * 10 + ch;
+    }
+    return temp_num;
 }
