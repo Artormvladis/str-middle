d7c3b49e6d34506a92a1230426ebeddb3f8c31f8
Artormvladis <112126702+Artormvladis@users.noreply.github.com>
2022-12-26 23:38:13 +0300

    Update middle_str.h

--- middle_str.h
+++ middle_str.h
@@ -3,6 +3,12 @@
 #include <iostream>
 #include <string.h>
 using namespace std;
+long long itc_len(string str1);
+int itc_find_str(string str1, string str2);
+string itc_slice_str(string str, int a1, int a2);
+string itc_covert_num(int a1, int a2);
+string itc_num_to_str(long long a1);
+long long str_to_num(string a1);
 bool itc_isDigit(unsigned char a1);
 unsigned char itc_toUpper(unsigned char a1);
 unsigned char itc_changeCase(unsigned char a1);
