bba12b9435c322c2cf889a1b216489a8ed33208f
Artormvladis <112126702+Artormvladis@users.noreply.github.com>
2022-12-26 20:42:27 +0300

    Update easy_func.cpp

--- easy_func.cpp
+++ easy_func.cpp
@@ -6,20 +6,6 @@ long long itc_len(string str1) {
     kol++;
   return kol;
 }
-int itc_find_str(string str1, string str2) {
-  long long len1=itc_len(str1);
-  long long len2=itc_len(str2);
-  for (int i=0; i<len1; i++) {
-    if (str1[i]==str2[0]) {
-      long long a1=0;
-      for (long long r = i; r < len1 && a1 < len2; r++, a1++){
-                if (str1[r] != str2[a1]) return 0;
-            }
-        }
-    }
-    return -1;
-}
-
 string itc_slice_str(string str, int a1, int a2){
     string res="";
     if (a2>= itc_len(str))
