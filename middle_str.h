4c3d76e37d5614c295cc00eac02ffa436520f839
Artormvladis <112126702+Artormvladis@users.noreply.github.com>
2022-12-26 23:44:52 +0300

    Update middle_str.h

--- middle_str.h
+++ middle_str.h
@@ -3,23 +3,23 @@
 #include <iostream>
 #include <string.h>
 using namespace std;
-long long itc_len(string str1);
+long long itc_len(string str);
 int itc_find_str(string str1, string str2);
-string itc_slice_str(string str, int a1, int a2);
-string itc_covert_num(int a1, int a2);
-string itc_num_to_str(long long a1);
-long long str_to_num(string a1);
-bool itc_isDigit(unsigned char a1);
-unsigned char itc_toUpper(unsigned char a1);
-unsigned char itc_changeCase(unsigned char a1);
-bool itc_compare(string a1, string a2);
+string itc_slice_str(string str, int start, int endd);
+bool itc_isDigit(unsigned char c);
+unsigned char itc_toUpper(unsigned char c);
+unsigned char itc_changeCase(unsigned char c);
+bool itc_compare(string s1, string s2);
 int itc_countWords(string str);
 string itc_maxCharWord(string str);
 char itc_sameChar(string str);
-bool itc_isFirstInSecond(string a1, string a2);
-string itc_Cezar(string str, int a1d);
+bool itc_isFirstInSecond(string s1, string s2);
+string itc_Cezar(string str, int k);
 string itc_rmFreeSpace(string str);
 bool itc_isIp(string str);
 string itc_DecToBin(string str);
-string itc_decToBase(int a1, int a2);
+string itc_decToBase(int num, int base);
+string itc_covert_num(int number, int ss);
+string itc_num_to_str(long long num);
+long long str_to_num(string temp);
 #endif // MIDDLE_STR_H_INCLUDED
