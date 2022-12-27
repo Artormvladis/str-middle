c9b18f178678b8b44d9890edc74cce13c7b17f78
Artormvladis <112126702+Artormvladis@users.noreply.github.com>
2022-12-27 11:03:16 +0300

    Update middle_func2.cpp

--- middle_func2.cpp
+++ middle_func2.cpp
@@ -34,30 +34,6 @@ int itc_countWords(string str)
     return c + 1;
 }
 
-string itc_maxCharWord(string str)
-{
-    long long max = 0, len = itc_len(str);
-    string a, b;
-    if (itc_countWords(str) <= 1) return "error";
-    for (int i = 0; i < len; ++i)
-    {
-        if (((str[i] >= 'A') && (str[i] <= 'Z')) || ((str[i] >= 'a') && (str[i] <= 'z')))
-            a += str[i];
-        else if (itc_len(a) > max)
-        {
-            max = itc_len(a);
-            b = a;
-            a = "";
-        }
-        else
-            a = "";
-    }
-    if (itc_len(a) > max)
-        b = a;
-    if (!itc_len(b)) return "error";
-    return b;
-}
-
 char itc_sameChar(string str)
 {
     for (long long i = 0; str[i] != '\0'; ++i)
